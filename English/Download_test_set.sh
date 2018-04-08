ae4ea31a8dee864005b14bfc5242db6adbcec222
Hyunyoung2 <hyun02.engineer@gmail.com>
2018-04-08 19:05:01 +0900

    change the structure of dir and file

--- English/Download_test_set.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Author : Hyunyoung2
-
-# To Download a set of syntactic and semantic test for word2vec
-# But This test set is just unigram 
-
-wget -c  http://www.fit.vutbr.cz/~imikolov/rnnlm/word-test.v1.txt
- 
--- English/normalized.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Author : hyunyoung2 
-
-IN_FILE="word-test.v1.txt"
-OUT_FILE="word-test.v1.normalized.txt"
-
-sed -e 's/	/ /g' ./$IN_FILE > ./$OUT_FILE
-
